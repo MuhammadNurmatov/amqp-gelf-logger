0e7794f9c2737c35f921dc56c0c93a8b90317387
muhammad6661 <nurmatov.muhammad000@gmail.com>
2025-06-04 15:02:22 +0500

    removed static

--- src/Services/UdpSocketService.php
+++ src/Services/UdpSocketService.php
@@ -11,33 +11,16 @@ class UdpSocketService implements AmqpGelfServiceContract
     private $socket = null;
     public array $config;
 
-    public bool $bound = false;
     public function __construct(array $config)
     {
         $this->config = $config;
         $this->socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-    }
-
-    private function bind(): void
-    {
-        $localPort = $this->config['local_port'] ?? 0;
-        $localHost = $this->config['local_host'] ?? '0.0.0.0';
         socket_set_option($this->socket, SOL_SOCKET, SO_SNDBUF, $this->config['max_buffer'] ?? 1400);
-        if (!socket_bind($this->socket, $localHost, $localPort)) {
-            $error = socket_strerror(socket_last_error($this->socket));
-            throw new RuntimeException("Failed to bind UDP socket on port {$localPort}: {$error}");
-        }
-
-        $this->bound = true;
     }
 
 
     public function send(mixed $message): bool|int
     {
-        if (!$this->bound) {
-            $this->bind();
-        }
-
        return  socket_sendto(
             $this->socket,
              $message,
